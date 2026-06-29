5d3febc48be84ea0bca000532023d2c0861c06cf
jamesjimjimbo <271764430+jamesjimjimbo@users.noreply.github.com>
2026-06-29 14:27:43 -0700

    Update LeaderboardView.js

--- src/components/LeaderboardView.js
+++ src/components/LeaderboardView.js
@@ -135,8 +135,8 @@ export default function LeaderboardView({ leaderboard, currentUserId, leagueMemb
           ) : null;
 
           const wager = pick.wager ?? 1;
-          const payout = correct && oddsValue ? wager * oddsValue : 0;
-
+          const payout = correct && oddsValue ? (wager * oddsValue) - wager : 0;
+          
           // Round label for knockout matches
           const roundLabel = m.isKnockout ? m.id.split('-')[0] : null;
 
